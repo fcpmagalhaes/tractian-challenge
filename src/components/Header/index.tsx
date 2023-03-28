81f08ac05c14eaf24502d6b440c0be368300e057
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-03-28 19:36:00 -0300

    create header and style

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import { Welcome } from '../Welcome'
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img className={styles.headerLogo} src="/images/tractian-logo.svg" alt="Tractian Logo"/>
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+        <Welcome/>
+      </div>
+    </header>
+  )
+}
