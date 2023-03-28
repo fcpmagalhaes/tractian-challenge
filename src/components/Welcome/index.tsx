26d98b0f0ead02e31d2eb8d85d6d01d3aeda1375
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-03-28 19:58:38 -0300

    create welcome component to show user logged

---
+++ src/components/Welcome/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { apiGH } from '@/services/axios/index';
+import styles from './styles.module.scss';
+
+type User = {
+  name: string;
+  avatar: string;
+};
+
+type ProfileResponse = {
+  name: string;
+  avatar_url: string; 
+};
+
+export function Welcome() {
+  const [user, setUser] = useState<User>({} as User);
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await apiGH.get<ProfileResponse>('/fcpmagalhaes');
+        console.log('user', user);
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        });
+      } catch(err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+      <div className={styles.container}>
+        <strong>{user.name}</strong>
+        <img src={user.avatar} alt="Foto de perfil" />
+      </div>
+  )
+}
