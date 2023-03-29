6406654cb7e5576e43d460fa03d336a03e803f16
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-03-29 20:29:33 -0300

    Update main pages component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,13 @@
-import { useEffect, useState } from "react"
-import { api } from "@/services/axios/index"
-import styles from '@/styles/home.module.scss';
 import Head from 'next/head';
-import { Button } from 'antd';
+import { LayoutContainer } from '@/components/LayoutContainer/index';
+import { Header } from '@/components/Header';
 
 export default function Home() {
-
-  type User = {
-    companyId: number;
-    email: string;
-    id: number;
-    name: string;
-    unitId: number;
-  };
-
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    chama();
-  },[]);
-
-  async function chama() {
-    const response = await api.get<User[]>('/users');
-    setUsers(response.data);
-  }
-
   return (
     <>
     <Head><title>TRACTIAN | Painel de Gerência</title></Head>
-      <h1 className={styles.title}>hello <span>world</span></h1>
-      <Button type="primary">Button</Button>
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Company</th>
-              <th>Unit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(user => {
-              return (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-                  <td>{user.companyId}</td>
-                  <td>{user.unitId}</td>
-                </tr>
-            )})}
-          </tbody>
-        </table>
+    <Header/>
+    <LayoutContainer/>
     </>
   )
 }
