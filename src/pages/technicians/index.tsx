f6d1fb5613b2de9153e65aa1cfa206b066fd1871
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-03-30 02:15:37 -0300

    update technicians page to get users data

--- src/pages/technicians/index.tsx
+++ src/pages/technicians/index.tsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react"
+import { api } from "@/services/axios/index"
+
 export default function Technicians() {
-  return(
-    <h1>Technicians</h1>
+   type User = {
+    companyId: number;
+    email: string;
+    id: number;
+    name: string;
+    unitId: number;
+  };
+
+  const [users, setUsers] = useState<User[]>([]);
+  
+  useEffect(() => {
+    async function getTechnicians() {
+      const response = await api.get<User[]>('/users');
+      setUsers(response.data);
+    };
+    getTechnicians();
+  },[]);
+
+  return (
+    <>
+      <h1>Técnicos</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Company</th>
+            <th>Unit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => {
+            return (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.companyId}</td>
+                <td>{user.unitId}</td>
+              </tr>
+          )})}
+        </tbody>
+      </table>
+    </>
   )
 }
