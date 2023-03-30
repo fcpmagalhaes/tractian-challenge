761fb2cb1f26b97bc09c7f2d39fdff1fbffacd7a
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-03-30 02:16:25 -0300

    clear home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
-import Head from 'next/head';
-import { LayoutContainer } from '@/components/LayoutContainer/index';
-import { Header } from '@/components/Header';
 
 export default function Home() {
   return (
     <>
-    <Head><title>TRACTIAN | Painel de Gerência</title></Head>
-    <Header/>
-    <LayoutContainer/>
+    <h1>Home</h1>
     </>
   )
 }
