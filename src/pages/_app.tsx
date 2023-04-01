6ad8d9e8af603876890ff356094420f6c9036c21
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-04-01 17:42:15 -0300

    Add session provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,22 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Header } from '@/components/Header';
 import { LayoutContainer } from '@/components/LayoutContainer';
+// import { Provider } from "react-redux";
+// import store from "@/store2";
+import { SessionProvider } from 'next-auth/react'
+
 import '@/styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head><title>TRACTIAN | Painel de Gerência</title></Head>
-      <Header/>
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <LayoutContainer contentPage={<Component {...pageProps} />}/>
-      </div>
-    </>
+    <SessionProvider session={pageProps.session}>
+      {/* <Provider store={store}> */}
+        <Head><title>TRACTIAN | Painel de Gerência</title></Head>
+        <Header/>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <LayoutContainer contentPage={<Component {...pageProps} />}/>
+        </div>
+      {/* </Provider> */}
+    </SessionProvider>
   )
 }
