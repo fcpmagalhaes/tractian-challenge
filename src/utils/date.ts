4e508e1dec9ef5b88c4e7dc8f1a2082d6e42ee68
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-04-10 09:01:23 -0300

    Add hour to format date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -9,5 +9,8 @@ export function formatDate(date: Date) {
   return (
     addZero(date.getDate()).toString() + "/" +
     addZero(date.getMonth()+1).toString() + "/" + 
-    date.getFullYear());
+    date.getFullYear() + "\t" +
+    addZero(date.getHours()) + "h" +
+    addZero(date.getMinutes()) + "min"
+  );
 }
