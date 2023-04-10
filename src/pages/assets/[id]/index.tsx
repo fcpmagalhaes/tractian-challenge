341af6885f420219712a4082786dc581fc242eff
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-04-10 09:12:05 -0300

    Finish add charts

--- src/pages/assets/[id]/index.tsx
+++ src/pages/assets/[id]/index.tsx
@@ -4,10 +4,18 @@ import { api } from "@/services/axios/index";
 import styles from "./styles.module.scss";
 import { setStatusData } from '@/utils/status';
 import { formatDate } from '@/utils/date';
+import { donutChart, BarChart } from '@/utils/chartDefault';
 
 import { Image, Divider, Timeline, Descriptions, Badge, Tag, List, Avatar } from 'antd';
 import Link from 'next/link';
 
+import Highcharts, { chart } from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import highchartsMore from "highcharts/highcharts-more";
+import solidGauge from "highcharts/modules/solid-gauge";
+
+highchartsMore(Highcharts);
+solidGauge(Highcharts);
 
 type Health = {
   status: string;
@@ -59,6 +67,9 @@ export default function Asset() {
   const [asset, setAsset] = useState<Asset>();
   const [healthHistory, setHealthHistory] = useState<HealthHistory[]>([]);
   const [status, setStatus] = useState<Status>({} as Status);
+  const [lastUptime, setLastUptime] = useState('');
+  const [metricChart, setMetricChart] = useState(BarChart);
+  const [healthChart, setHealthChart] = useState(donutChart);
 
   async function getTechnician() {
     const response = await api.get<Asset>(`/assets/${id}`);
@@ -68,12 +79,11 @@ export default function Asset() {
   function createHealthHistory(healthData: Array<Health>) {
     const healtMapped = healthData.map((item) => {
       const dateFormated = formatDate(new Date(item.timestamp));
-      let label = setStatusData(item.status);
-      
+      const {translatedLabel, colorStatus} = setStatusData(item.status);
       return {
-        label: label.translatedLabel,
+        label: translatedLabel,
         children: dateFormated,
-        color: label.colorStatus,
+        color: colorStatus,
       }
     });
 
@@ -88,23 +98,43 @@ export default function Asset() {
     if (asset) {
       createHealthHistory(asset.healthHistory);
       setStatus(setStatusData(asset.status));
+      setLastUptime(formatDate(new Date(asset.metrics.lastUptimeAt)));
+
+      const seriesHealth = {
+        series: [{
+          name: 'Nível',
+          data: [{
+            color: '#3498db',
+            radius: '112%',
+            innerRadius: '88%',
+            y: asset.healthscore,
+          }],
+        }]
+      };
+
+      const seriesMetrics = {
+        series: [
+          {
+            name: 'Tempo Total de Atividade',
+            data: [asset.metrics.totalUptime],
+            color: '#6AA5E7',
+            index: 2
+          },
+          {
+            name: 'Tempo Total de Coleta',
+            data: [asset.metrics.totalCollectsUptime],
+            color: '#6C6DE3',
+            index: 0
+          }
+        ]
+      };
+      
+      setHealthChart({...healthChart, ...seriesHealth});
+      setMetricChart({...metricChart, ...seriesMetrics});
+
     }
   },[asset]);
 
-  const data = [
-    {
-      title: 'Ant Design Title 1',
-    },
-    {
-      title: 'Ant Design Title 2',
-    },
-    {
-      title: 'Ant Design Title 3',
-    },
-    {
-      title: 'Ant Design Title 4',
-    },
-  ];
   
   return ( 
     asset ? (
@@ -116,6 +146,11 @@ export default function Asset() {
             fallback="/images/asset-img-error.png"
             alt="asset image"
           />
+          <Divider />
+          <HighchartsReact highcharts={Highcharts} options={healthChart} />
+          <Divider />
+          <HighchartsReact highcharts={Highcharts} options={metricChart} />
+          
           {/* <Divider orientation="center">Presets</Divider> */}
           <Divider />
 
@@ -124,11 +159,22 @@ export default function Asset() {
             <Descriptions.Item label="Sensores">
               {asset.sensors.map((sensor: string, index: number) => <Tag color='processing' key={index}>{sensor}</Tag>)}
             </Descriptions.Item>
-            <Descriptions.Item label="Status" span={3}>
+            <Descriptions.Item label="Status" span={2}>
               <Badge color={status.colorStatus} text={status.translatedLabel} />
             </Descriptions.Item>
+            <Descriptions.Item label="Último Tempo de Atividade">{lastUptime}</Descriptions.Item>
             <Descriptions.Item label="Empresa">{asset.companyId}</Descriptions.Item>
             <Descriptions.Item label="Unidade">{asset.unitId}</Descriptions.Item>
+            <Descriptions.Item label="Temperatura Máxima">
+              {asset.specifications.maxTemp ? `${asset.specifications.maxTemp}°C`: '-'}
+            </Descriptions.Item>
+            <Descriptions.Item label="Potência">
+              {asset.specifications.power ? `${asset.specifications.power}kWh`: '-'}
+            </Descriptions.Item>
+            <Descriptions.Item label="RPM">
+              {asset.specifications.rpm ? `${asset.specifications.rpm}`: '-'}
+              {asset.specifications.rpm}
+            </Descriptions.Item>
           </Descriptions>
 
           <Divider />
